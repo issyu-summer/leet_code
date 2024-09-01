17d6c476343d5a9e21132a0a4bfb1f454fba6589
wangyu <2119635988@qq.com>
2024-09-01 17:46:24 +0800

    move zero to last

--- move_zeores.go
+++ move_zeores.go
@@ -3,3 +3,14 @@ package main
 func main() {
 
 }
+
+func moveZeroes(nums []int) {
+	i, j := 0, 0
+	for j < len(nums) {
+		if nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		j++
+	}
+}
